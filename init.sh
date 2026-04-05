7c101883373fb9a7f994aacfb25d5bda2d902649
Frozen12 <31291110+Frozen12@users.noreply.github.com>
2026-04-06 04:49:13 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
+# Ensure required dirs
 mkdir -p /data/moltis/config
 
 # Start qmd
 qmd &
 
-# Start Moltis
+# Start main service
 exec moltis --bind 0.0.0.0 --port ${PORT:-13131}
